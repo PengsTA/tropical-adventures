061b858ba68bb4c4ee43e25949ac959cb2881240
BBGoat <bbgoat@vip.qq.com>
2026-09-19 06:34:45 +0800

    v26.09.19

--- modinfo.lua
+++ modinfo.lua
@@ -66,7 +66,7 @@ name = pub_dev(en_zh("Tropical Adventures|Ship of Theseus", "热带冒险|忒修
     en_zh("Tropical Adventures|Dev", "热带冒险|开发版"))
 
 author = "Peng et al."
-version = "26.03.28"
+version = "26.09.19"
 forumthread = ""
 api_version = 10
 priority = -100
